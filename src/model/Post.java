e0f86e7328cc387267940a31ce4dc934628ddc03
Jamoliddin Saydirasulov <154518209+Jamoliddin007@users.noreply.github.com>
2024-07-18 22:45:50 +0500

    jamol

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,4 +1,48 @@
 package model;
 
+import java.util.UUID;
+
 public class Post {
+    private UUID id;
+    private UUID UserId;
+    private String post;
+
+    public Post() {
+        this.id = id;
+    }
+
+    public Post(UUID userId, String post) {
+        this();
+        UserId = userId;
+        this.post = post;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(UUID userId) {
+        UserId = userId;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    @Override
+    public String toString() {
+        return post;
+    }
 }
--- src/service/PostService.java
+++ src/service/PostService.java
@@ -1,4 +1,90 @@
 package service;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import model.Post;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class PostService {
+    private static final String FILE_PATH = "src\\file\\posts.json";
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    //TODO -> add();
+    public Post add(Post post) {
+        List<Post> posts = fileRead();
+        posts.add(post);
+        fileWrite(posts);
+        return post;
+    }
+
+    //TODO -> update();
+    public boolean update(UUID postId, String newPostContent) {
+        List<Post> posts = fileRead();
+        for (Post post : posts) {
+            if (post.getId().equals(postId)) {
+                post.setPost(newPostContent);
+                fileWrite(posts);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //TODO -> delete();
+    public boolean delete(UUID postId) {
+        List<Post> posts = fileRead();
+        for (Post post : posts) {
+            if (post.getId().equals(postId)) {
+                posts.remove(post);
+                fileWrite(posts);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //TODO -> list();
+    public List<Post> list(UUID id) {
+        ArrayList<Post> postArrayList = fileRead();
+        ArrayList<Post> posts = new ArrayList<>();
+        for (Post post : postArrayList) {
+            if (post.getUserId().equals(id)) {
+                posts.add(post);
+            }
+        }
+        return posts;
+
+    }
+
+    //TODO -> fileWrite();
+    private void fileWrite(List<Post> posts) {
+        try (Writer writer = new FileWriter(FILE_PATH)) {
+            gson.toJson(posts, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //TODO -> fileRead();
+    private ArrayList<Post> fileRead() {
+        try (Reader reader = new FileReader(FILE_PATH)) {
+            Type postListType = new TypeToken<ArrayList<Post>>() {
+            }.getType();
+            ArrayList<Post> posts = gson.fromJson(reader, postListType);
+            if (posts == null) {
+                return new ArrayList<>();
+            }
+            return posts;
+        } catch (FileNotFoundException e) {
+            return new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
 }
