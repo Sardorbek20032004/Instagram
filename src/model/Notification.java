12b2c287d4a1eacd99cec0386ba04191e2881c25
Abdulaziz_java <rakhimoff1505@gmail.com>
2024-07-18 22:02:20 +0500

    Abdulaziz ok

--- src/model/Notification.java
+++ src/model/Notification.java
@@ -1,4 +1,47 @@
 package model;
-
+import java.util.UUID;
 public class Notification {
+    private UUID fromUserId;
+    private UUID toUserId;
+    private boolean status;
+    private UUID postId;
+
+    public Notification(UUID fromUserId, UUID toUserId, boolean status, UUID postId) {
+        this.fromUserId = fromUserId;
+        this.toUserId = toUserId;
+        this.status = status;
+        this.postId = postId;
+    }
+
+    public UUID getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(UUID fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public UUID getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(UUID toUserId) {
+        this.toUserId = toUserId;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public UUID getPostId() {
+        return postId;
+    }
+
+    public void setPostId(UUID postId) {
+        this.postId = postId;
+    }
 }
--- src/service/NotificationService.java
+++ src/service/NotificationService.java
@@ -1,4 +1,86 @@
 package service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import model.Notification;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class NotificationService {
+    private static final String PATH = "src/file/notifications.json";
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
+
+    public void addNotification(Notification notification) {
+       ArrayList<Notification> notifications = fileRead();
+       notifications.add(notification);
+       fileWrite(notifications);
+    }
+
+    public void markAsRead(UUID notificationId) {
+        ArrayList<Notification> notifications = fileRead();
+        for (Notification notification : notifications) {
+            if (notification.getFromUserId().equals(notificationId)) {
+                notification.setStatus(true);
+                break;
+            }
+        }
+        fileWrite(notifications);
+    }
+
+    public void userViewNotifications(UUID userId) {
+        ArrayList<Notification> notifications = fileRead();
+        for (Notification notification : notifications) {
+            if (notification.getToUserId().equals(userId)) {
+                notification.setStatus(false);
+            }
+        }
+        fileWrite(notifications);
+    }
+
+    public List<Notification> getNotifications(UUID id) {
+        ArrayList<Notification> notifications = fileRead();
+        ArrayList<Notification> notificationArrayList = new ArrayList<>();
+        for (Notification notification: notifications){
+            if (notification.getFromUserId().equals(id) && notification.isStatus()){
+                notificationArrayList.add(notification);
+            }
+        }
+        return notificationArrayList;
+    }
+
+
+    private void fileWrite(List<Notification> notifications) {
+        String json = gson.toJson(notifications);
+        try (FileWriter writer = new FileWriter(PATH)) {
+            writer.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<Notification> fileRead() {
+        StringBuilder json = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                json.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Type typeToken = new TypeToken<List<Notification>>() {}.getType();
+        ArrayList<Notification> notifications = gson.fromJson(json.toString(), typeToken);
+        if (notifications == null) {
+            notifications = new ArrayList<>();
+        }
+        return notifications;
+    }
 }
